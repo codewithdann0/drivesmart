5a469db7f7b869cc88b7c15f09056aada9a7246d
codewithdann0 <codewithdann028@gmail.com>
2024-08-24 00:23:36 -0700

    the admin dashboardfortest deleted

--- pages/api/admin/dashboard.ts
+++
@@ -1,6 +0,0 @@
-import { requireAuth } from '../../../lib/auth';
-
-export default requireAuth(async (req, res) => {
-    // Your API logic here
-    res.status(200).json({ message: 'Welcome to the admin dashboard' });
-});
